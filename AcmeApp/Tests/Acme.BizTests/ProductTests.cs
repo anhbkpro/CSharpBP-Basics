988789d133faed46973ad6fb34f281ebbca4a07c
Lai Ngoc Anh <lai.anh@outlook.com>
2017-09-09 15:48:10 +0700

    building good classes

---
+++ AcmeApp/Tests/Acme.BizTests/ProductTests.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Acme.Biz;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Acme.Biz.Tests
+{
+    [TestClass()]
+    public class ProductTests
+    {
+        [TestMethod()]
+        public void SayHelloTest()
+        {
+            //Arrange
+            var currentProduct = new Product(1, "Saw", "15 inch steel blade hand saw");
+            currentProduct.ProductVendor.CompanyName = "ABC Corp";
+            var expected = "Hello Saw (1): 15 inch steel blade hand saw";
+
+            //Act
+            var actual = currentProduct.SayHello();
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod()]
+        public void Product_Null()
+        {
+            //Arrange
+            Product currentProduct = null;
+            var companyName = currentProduct?.ProductVendor?.CompanyName;
+
+            string expected = null;
+
+            //Act
+            var actual = companyName;
+
+            //Assert
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
